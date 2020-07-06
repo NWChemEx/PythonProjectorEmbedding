6fac5378eef9f876e07d4b5279921a1e4ef12099
Jonathan Waldrop <relik107@gmail.com>
2020-07-06 16:35:22 -0500

    initial updating

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-# __init__.py
+""" __init__.py """
 from projectorEmbedding.embed_utils import make_dm
 from projectorEmbedding.embed_utils import flatten_basis
 from projectorEmbedding.embed_utils import purify
@@ -6,5 +6,3 @@ from projectorEmbedding.embed_utils import purify
 from projectorEmbedding.embed_proc import mulliken_partition
 from projectorEmbedding.embed_proc import spade_partition
 from projectorEmbedding.embed_proc import embedding_procedure
-
-from projectorEmbedding.embed_prev import embedding_procedure as old_procedure

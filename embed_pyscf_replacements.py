d3de56d986231ad66e74558ab9efcbcc37683862
Jonathan M. Waldrop <relik107@gmail.com>
2021-09-08 17:30:14 -0500

    energy_elec more specific

--- embed_pyscf_replacements.py
+++ embed_pyscf_replacements.py
@@ -1,17 +1,26 @@
 import numpy as np
 from pyscf.lib import logger, NPArrayWithTag
 
+def _check_dims_then_einsum(mat, dm):
+    if mat.ndim == 2 and dm.ndim == 2:
+        return np.einsum('ij,ji->', mat, dm)
+    elif mat.ndim == 2 and dm.ndim == 3:
+        return np.einsum('ij,aji->', mat, dm)
+    elif mat.ndim == 3 and dm.ndim == 2:
+        return np.einsum('aij,ji->', mat, dm * 0.5)
+    elif mat.ndim == 3 and dm.ndim == 3:
+        return np.einsum('aij,aji->', mat, dm)
+    else:
+        raise RuntimeError(f"Unexpected dims: {mat.ndim}, {dm.ndim}")
+        
+
 def energy_elec(mf, dm=None, h1e=None, vhf=None):
     if dm is None: dm = mf.make_rdm1()
     if h1e is None: h1e = mf.get_hcore()
     if vhf is None or (isinstance(vhf, NPArrayWithTag) and getattr(vhf, 'ecoul', None) is None):
         vhf = mf.get_veff(mf.mol, dm)
 
-    if isinstance(dm, np.ndarray) and dm.ndim == 2:
-        dm = np.array((dm*.5, dm*.5))
-    if isinstance(h1e, np.ndarray) and h1e.ndim == 2:
-        h1e = np.array((h1e, h1e))
-    e1 = np.einsum('aij,aji->', h1e, dm)
+    e1 = _check_dims_then_einsum(h1e, dm)
 
     if hasattr(vhf, 'ecoul'):
         e2 = vhf.ecoul + vhf.exc
@@ -21,9 +30,7 @@ def energy_elec(mf, dm=None, h1e=None, vhf=None):
         mf.scf_summary['exc'] = vhf.exc.real
         logger.debug(mf, 'E1 = %s  Ecoul = %s  Exc = %s', e1, vhf.ecoul, vhf.exc)
     else:
-        if isinstance(vhf, np.ndarray) and vhf.ndim == 2:
-            vhf = np.array((vhf, vhf))
-        e2 = np.einsum('aij,aji->', vhf, dm) * 0.5
+        e2 = _check_dims_then_einsum(vhf, dm) * 0.5
 
         mf.scf_summary['e1'] = e1.real
         mf.scf_summary['e2'] = e2.real
